805150ee0a1c05caa1486d1a4cd9b8701b5ef439
AlexanderIon <tov.sandro@mail.ru>
2022-12-08 14:30:02 +0300

    add lint

--- src/js/appGame.js
+++ src/js/appGame.js
@@ -4,15 +4,13 @@ document.addEventListener('DOMContentLoaded', () => {
   const fiedls = document.querySelector('.fieldGame');
   const btn = document.querySelector('.activeGoblinBtn');
   const gonlin = new Goblin(fiedls);
- 
+
   btn.addEventListener('click', () => {
     // gonlin.movingdGoblin();
     // console.log('click')
-    statusGame = true;
+    // statusGame = true;
     const myInterval = setInterval(() => {
       gonlin.movingdGoblin();
-      
-    },1500)
-    e
+    }, 1500);
   });
 });
